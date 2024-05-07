a5f3354c2b52ea405e0da09412ea42fc7ae55ace
codebyhannah <firekatburns@gmail.com>
2024-05-07 15:10:28 -0600

    Fixed add item to cart and empty cart error.

--- src/js/product.js
+++ src/js/product.js
@@ -4,7 +4,13 @@ import ProductData from "./ProductData.mjs";
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  // To avoid overwriting in localStorage, either use a different key for each item, or place items in an array. Get array from local storage, push to array, and set local storage to updated array.
+  let productsInCart = JSON.parse(localStorage.getItem("so-cart"));
+  if (!localStorage.getItem("so-cart")) {
+    productsInCart = [];
+  }
+  productsInCart.push(product);
+  setLocalStorage("so-cart", productsInCart);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
