fae1395ca470598010b414dbce56110b300463b8
codebyhannah <firekatburns@gmail.com>
2024-05-25 22:54:59 -0600

    made duplicate items in cart increment quantity instead of appearing multiple times in the list.

--- src/js/ShoppingCart.mjs
+++ src/js/ShoppingCart.mjs
@@ -1,20 +1,21 @@
 import { getLocalStorage, setLocalStorage, renderListWithTemplate } from "./utils.mjs";
 
-function cartItemTemplate(item) {
+function cartItemTemplate(cartItem) {
+  
     return `<li class="cart-card divider">
     <a href="#" class="cart-card__image">
       <img
-        src="${item.Image}"
-        alt="${item.Name}"
+        src="${cartItem.item.Images.PrimarySmall}"
+        alt="${cartItem.item.Name}"
       />
     </a>
     <a href="#">
-      <h2 class="card__name">${item.Name}</h2>
+      <h2 class="card__name">${cartItem.item.Name}</h2>
     </a>
-    <p class="cart-card__color">${item.Colors[0].ColorName}</p>
-    <p class="cart-card__quantity">qty: 1</p>
-    <p class="cart-card__price">$${item.FinalPrice}</p>
-    <button class="cart-card__remove-item" type="button" data-id="${item.Id}">X</button>
+    <p class="cart-card__color">${cartItem.item.Colors[0].ColorName}</p>
+    <p class="cart-card__quantity">qty: <span class="quantity">${cartItem.quantity}</span></p>
+    <p class="cart-card__price">$${cartItem.item.FinalPrice}</p>
+    <button class="cart-card__remove-item" type="button" data-id="${cartItem.item.Id}">X</button>
   </li>`;
 }
 
@@ -47,12 +48,41 @@ export default class ShoppingCart {
         setLocalStorage(this.listSource, updatedCart);
         this.renderCartContents();
     }
+
+    getCartItemsWithQuantitiesList(cartItems) {
+      let cartItemOccurences = [];
+          cartItems.forEach(item => {
+            let filtered = cartItems.filter((fItem) => (fItem.Id == item.Id));
+            cartItemOccurences.push(filtered);
+          });
+          // Pretty sure something in this method could be condensed, do that later.
+          let cartQuantities = [];
+          cartItemOccurences.forEach(quantityAr => {
+            let done = false;
+            cartQuantities.forEach(doneQuantityAr => {
+              if(quantityAr[0].Id == doneQuantityAr[0].Id) {
+                done = true;
+              }
+            });
+            if(!done) {
+              cartQuantities.push(quantityAr);
+            }
+          });
+          
+          let cartItemsWithQuantities = [];
+          cartQuantities.forEach(itemAr => {
+            let cartItem = {item: itemAr[0], quantity: itemAr.length}
+            cartItemsWithQuantities.push(cartItem);
+          });
+          return cartItemsWithQuantities;
+    }
       
     renderCartContents() {
         if (localStorage.getItem(this.listSource)) {
           const cartItems = getLocalStorage(this.listSource);
-      
-          this.renderList(cartItems);
+
+          this.renderList(this.getCartItemsWithQuantitiesList(cartItems));
+          //this.renderList(cartItems);
       
           const cartFooter = document.querySelector(".cart-footer");
           if (cartItems.length == 0) {
