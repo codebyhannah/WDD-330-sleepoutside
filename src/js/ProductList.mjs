96077198bf535197a2f7c615335c39d01c1ac90a
codebyhannah <firekatburns@gmail.com>
2024-05-29 21:00:40 -0600

    removed unnecessary comment

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -27,20 +27,3 @@ export default class ProductList {
         renderListWithTemplate(productCardTemplate, this.listElement, list);
     }
 }
-/* 
-Remove extra products
-    tents.json has more tents than we currently need.
-    Write a method in productList.js that will filter our list of products to just the four (4) we need.
-    Use that method to show only those four (4) tents.
-
-filter list how? IDs?
-
-array.filter works like this
-
-array.filter(callback);
-function callback(item) {
-    return bool-about-item;
-}
-
-so I could do something like == ID || == difID
-*/
